ccb309990a2c6d58eae874b9a462c6489dac22e3
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-02-28 22:29:01 -0800

    Time: 38 ms (62.26%), Space: 13.8 MB (81.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,26 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #We initialize our dp array.
-        #dp Index: this will be the step number we are currently on
-        #dp Value: This will be the number of unique ways we are able to get to this current step.
+        if n <= 2: return n
         
-        
-        #We initialize our dp array with dp[0] = 1 and dp[1] = 1. Only one way of getting to the top when there are zero
-        #Stairs, do nothing. Only one way to get to the top when there is only one step, take one step
-        
-        #Now we iterate through the rest of our array. We say that the number of ways to get to the current step
-        #is the number of ways from the previous step plus the number of ways from two steps ago. This is because
-        #Our only moves from those steps are to move one or two steps
-            
-        #We return the last step
-        
-    
-        dp = [0] * (n+1)
-        
-        dp[0] = 1
-        dp[1] = 1
-        
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i - 2]
-            
-        return dp[n]
+        prev1 = 1
+        prev2 = 2
+        current = 0
+        for i in range(2,n):
+            current = prev1 + prev2
+            prev1 = prev2
+            prev2 = current
+        return current
