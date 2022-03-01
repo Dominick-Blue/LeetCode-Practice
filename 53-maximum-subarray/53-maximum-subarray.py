ac0e085e1bc53960e069392fb82b0c46fc9a4e8b
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-02-28 20:25:27 -0800

    Time: 1083 ms (42.16%), Space: 27.8 MB (97.51%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # DP
-        # T: O(N)
-        # S: O(N)
+        # DP Approach - Kadane's Algorithm
+        # Time Complexity: O(n) & Space Complexity: O(1)
         
-        cache = [0] * len(nums)
-        cache[0] = nums[0]
+        current_sub = nums[0]
+        max_sub = nums[0]
         
-        for i in range(1, len(nums)):
-            cache[i] = max(nums[i] + cache[i-1], nums[i])
-            
-        return max(cache)
-        
+        for num in nums[1:]:
+            current_sub = max(num, current_sub + num)
+            max_sub = max(max_sub, current_sub)
+        return max_sub
