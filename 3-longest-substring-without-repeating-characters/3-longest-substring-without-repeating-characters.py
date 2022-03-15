d9893f38e72f47654f873fb3ce169aa31c667642
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-03-14 22:15:33 -0700

    Time: 49 ms (84.89%), Space: 14.4 MB (30.55%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,18 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        list_len = len(s)
-        max_len = 0
-        
+
+        list_length = len(s)
+        max_subarray = 0
+        window_start = 0
         char_map = {}
         
-        window_start = 0
-        
-        for window_end in range(list_len):
+        for window_end in range(list_length):
             if s[window_end] in char_map:
                 window_start = max(char_map[s[window_end]], window_start)
-            
-            max_len = max(max_len, window_end - window_start + 1)
+                
+            max_subarray = max(max_subarray, window_end - window_start + 1)
             char_map[s[window_end]] = window_end + 1
-            
-        return max_len
+        return max_subarray
