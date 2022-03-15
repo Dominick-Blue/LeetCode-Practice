57da3861f96d18fd575480b6199e133c265b231e
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-03-14 19:45:11 -0700

    Time: 117 ms (45.02%), Space: 15.3 MB (33.23%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,19 @@
+from collections import Counter
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-            
-        dict = {}
+#         count = {}
         
-        for index,num in enumerate(nums):
+#         for index,num in enumerate(nums):
+#             complement = target - num
+#             if complement in count:
+#                 return [index, count[complement]]
+#             count[num] = index
+        
+        count = {}
+        
+        for index, num in enumerate(nums):
             complement = target - num
-            if complement in dict:
-                return [dict[complement], index]
-            
-            dict[num] = index
+            if complement in count:
+                return [index, count[complement]]
+            count[num] = index
