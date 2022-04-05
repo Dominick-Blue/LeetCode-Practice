6088d46ef2c5ac041ae03ceaf922f71d8380a2ac
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-05 08:10:17 -0400

    Time: 75 ms (29.16%), Space: 13.8 MB (74.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,4 +5,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s) == Counter(t)
+        
+        if len(s) != len(t):
+            return False
+        
+        count_s, count_t = {}, {}
+        
+        for char in s:
+            if char in count_s:
+                count_s[char] += 1
+            else:
+                count_s[char] = 1
+        
+        for char in t:
+            if char in count_t:
+                count_t[char] += 1
+            else:
+                count_t[char] = 1
+                
+        for char in count_s:
+            if count_s[char] != count_t.get(char, 0):
+                return False
+        
+        return True
+                
+        
