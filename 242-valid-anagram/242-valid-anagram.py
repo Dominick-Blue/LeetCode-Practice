cfddca421f3e2086fa3bc18ec9de27f4e643c606
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-05 08:18:09 -0400

    Time: 88 ms (17.24%), Space: 14.8 MB (25.84%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,19 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
-        if len(s) != len(t):
-            return False
-        
-        count_s, count_t = {},{}
-        
-        for i in range(0, len(s)):
-            count_s[s[i]] = count_s.get(s[i], 0) + 1
-            count_t[t[i]] = count_t.get(t[i], 0) + 1
-        
-        for char in count_s:
-                if count_s[char] != count_t.get(char, 0):
-                    return False
-                
-        return True
-                
+        return sorted(s) == sorted(t)
