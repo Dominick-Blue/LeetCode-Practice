c7276c8423afcfb26fdc3c1986074d4f382c6464
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-11 21:38:13 -0400

    Time: 83 ms (33.15%), Space: 14 MB (96.41%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -7,7 +7,7 @@ class Solution(object):
         
         left, right = 0, len(s) - 1
         
-        while left < right: 
+        while left < right:
             if not s[left].isalnum():
                 left += 1
             elif not s[right].isalnum():
