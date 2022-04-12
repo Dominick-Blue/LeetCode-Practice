cb9526c5c87c159210b5ae4f08319959f820ee9e
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-11 21:22:59 -0400

    Time: 50 ms (64.02%), Space: 13.8 MB (99.75%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -5,31 +5,17 @@ class Solution(object):
         :rtype: bool
         """
         
-        l, r = 0, len(s) - 1
+        left, right = 0, len(s) - 1
         
-        while l < r:
-            while l < r and not self.alpha_num(s[l]):
-                l += 1
-            while r > l and not self.alpha_num(s[r]):
-                r -= 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
             
-            if s[l].lower() != s[r].lower():
+            if left < right and s[left].lower() != s[right].lower():
                 return False
-            l, r = l + 1, r - 1
+            
+            left, right = left + 1, right - 1
             
         return True
-    
-        
-        
-        
-    def alpha_num(self, c):
-        return ((ord('A') <= ord(c) <= ord('Z')) or 
-                ord('a') <= ord(c) <= ord('z') or
-                 ord('0') <= ord(c) <= ord('9'))
-        
-        
-            
-                      
-        
-                    
-        
