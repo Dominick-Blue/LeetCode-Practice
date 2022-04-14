0902b4e5159ce91b86aa828f2f99f9fe3d0252f0
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-14 12:38:44 -0400

    Time: 47 ms (88.20%), Space: 14.4 MB (30.07%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
-        char_set = set()
-        left = 0
-        result = 0
+
+        char_map = {}
+        left, result = 0,0
         
         for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-            char_set.add(s[right])
+            if s[right] in char_map:
+                left = max(char_map[s[right]], left)
+            
             result = max(result, right - left + 1)
+            char_map[s[right]] = right + 1
         return result
-                
