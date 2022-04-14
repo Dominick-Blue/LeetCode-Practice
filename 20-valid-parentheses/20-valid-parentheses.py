b7a06aec3f0e268f628bf5953be2f76fdd87b682
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-14 13:03:03 -0400

    Time: 11 ms (99.14%), Space: 13.7 MB (32.77%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,18 +1,30 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-    
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        if s[0] == ')' or s[0] == ']' or s[0] == '}':
+            return False
+        
         stack = []
         
-        mapping = {")":"(","}":"{","]":"["}
+        paren_map = {
+            ')':'(',
+            '}': '{',
+            ']':'['
+        }
+        
         
         for char in s:
-            if char in mapping:
-                top_element = stack.pop() if stack else '#'
-                
-                if mapping[char] != top_element:
+            if char in paren_map:
+                if stack and stack[-1] == paren_map[char]:
+                    stack.pop()
+                else:
                     return False
-                
             else:
                 stack.append(char)
         
         return not stack
+            
