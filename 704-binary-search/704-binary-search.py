fe02a982df04006511af7a678ba51f0091f9afc3
Dominick Blue <65993112+dominick-blue1@users.noreply.github.com>
2022-04-15 20:03:55 -0400

    Time: 209 ms (76.16%), Space: 15 MB (8.20%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -5,13 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-
-        lo = 0
-        hi = len(nums) - 1
+        
+        lo, hi = 0, len(nums) - 1
         
         while lo <= hi:
             mid = (lo + hi) // 2
-            
             if target < nums[mid]:
                 hi = mid - 1
             elif target > nums[mid]:
