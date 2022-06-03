3200c56aa7d5c4087e3bee1c57cfc0e57c410f6a
Dominick Blue <dominick.blue7@gmail.com>
2022-06-03 01:23:47 -0400

    Time: 4 ms (22.76%), Space: 42.2 MB (30.11%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Hash Table for mappings
+    private HashMap<Character, Character> mappings;
+    
+    // Initalize map with mappings
+    public Solution() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put(']', '[');
+        this.mappings.put('}', '{');
+    }
+    
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (this.mappings.containsKey(c)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+                
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
