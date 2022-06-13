a78dc3e01a32adfb2c92dd0437a35215ef792472
Dominick Blue <dominick.blue7@gmail.com>
2022-06-13 04:52:52 -0400

    Time: 57 ms (88.10%), Space: 16.6 MB (90.88%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -7,6 +7,6 @@ class Solution(object):
         intervals.sort()
         
         for i in range(len(intervals) - 1):
-            if intervals[i][1] > intervals[i+1][0]:
+            if intervals[i+1][0] < intervals[i][1]:
                 return False
         return True
